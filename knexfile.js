1f13a237be85dfca13dc79ba6ba94c564f7451f8
144mdgross <144.mdgross@gmail.com>
2017-05-17 10:50:48 -0600

    db, migrations, seeds in working order

--- knexfile.js
+++ knexfile.js
@@ -2,8 +2,30 @@
 
 module.exports = {
 
-  development: { },
-  test: { },
-  production: { },
-
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/classified_dev',
+    // pool: {
+    //   min: 2,
+    //   max: 10
+    // },
+    // migrations: {
+    //   tableName: 'knex_migrations'
+    // }
+  },
+test: {
+    client: 'pg',
+    connection: 'postgres://localhost/classified_test'
+    // pool: {
+    //   min: 2,
+    //   max: 10
+    // },
+    // migrations: {
+    //   tableName: 'knex_migrations'
+    // }
+  },
+  production: {
+    "client": "pg",
+    "connection": process.env.DATABASE_URL
+  }
 };
