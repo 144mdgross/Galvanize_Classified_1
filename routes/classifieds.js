ad8530fe02bb209f431bfb671ade83b56e07d663
144mdgross <144.mdgross@gmail.com>
2017-05-17 11:11:29 -0600

    Create and Read routes passing tests

--- routes/classifieds.js
+++ routes/classifieds.js
@@ -2,9 +2,45 @@
 'use strict';
 
 const express = require('express');
+const knex = require('../knex')
 
 const router = express.Router();
 
-// YOUR CODE HERE
+router.get('/', (req, res, next) => {
+  knex('classifieds')
+  .select('id', 'title', 'description', 'price', 'item_image')
+  .then(posts => {
+    res.json(posts)
+  })
+})
+
+router.get('/:id', (req, res, next) => {
+  knex('classifieds')
+    .select('id', 'title', 'description', 'price', 'item_image')
+    .where('id', req.params.id)
+    .then(post => {
+      res.json(post[0])
+    })
+})
+
+router.post('/', (req, res, next) => {
+  knex('classifieds')
+    .insert([{
+      title: req.body.title,
+      description: req.body.description,
+      price: req.body.price,
+      item_image: req.body.item_image
+    }], '*')
+      .then(post => {
+        let newPost = {
+          id: post[0].id,
+          title: post[0].title,
+          description: post[0].description,
+          price: post[0].price,
+          item_image: post[0].item_image
+        }
+        res.json(newPost)
+      })
+})
 
 module.exports = router;
